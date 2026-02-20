d2aad003af537ffba6ebe99c5a460397eb6e7da0
むにまる <munimaru62ooo@gmail.com>
2026-02-20 19:26:52 +0900

    [fix] fix standalone compilation errors by adding missing includes

--- Plugins/GameCoreFramework/Source/GameCoreFramework/Private/Movement/GCFMovementFunctionLibrary.cpp
+++ Plugins/GameCoreFramework/Source/GameCoreFramework/Private/Movement/GCFMovementFunctionLibrary.cpp
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #include "Movement/GCFMovementFunctionLibrary.h"
-
+#include "GCFShared.h"
 #include "Movement/GCFMovementConfigReceiver.h"
 #include "Movement/GCFLocomotionHandler.h"
 
